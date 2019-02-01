8d424507c18173fbfc85ff260dc50d5f26083497
Seth Treviño <sethtv@hotmail.com>
2019-01-31 18:52:24 -0600

    Actividad 1

---
+++ Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var v1 = "Esto es Swift";
+let c1 = 4;
+
+var cadena:String = String();
+
+cadena = "Hola mundo " + v1 ;
+
+print("El Contenido es : \(cadena)");
+
+print("El valor es constante es : \(c1)");
